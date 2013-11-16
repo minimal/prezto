6faac81c3a43f054885d1a5b60c1f34dd63d88f7
Chris McDevitt <christophermcdevitt@gmail.com>
2013-11-16 20:55:25 +0000

    Port most of my customisations to chris-settings module
    * Port still used things from .zshrc
    * copy over alias.zsh
    * c completion function
    * stop cursor syntax highlighters

---
+++ modules/chris-settings/init.zsh
@@ -0,0 +1,43 @@
+
+kernel=`uname`
+
+setopt interactivecomments
+
+# awesome!! e.g. $ git <up-arrow> => $ git log
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward
+# some keyboards use this:
+bindkey "^[OA" history-beginning-search-backward
+bindkey "^[OB" history-beginning-search-forward
+
+
+# functions
+function cmd_exists() {
+    command -v "$1" >/dev/null 2>&1;
+}
+
+function h () {
+    cd ~/$1
+}
+
+function c () {
+    _cd ~/code/$1;
+}
+
+if [ "$kernel" = 'Darwin' ]; then
+    export GOROOT=/usr/local/Cellar/go/1.0.3/
+    export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
+    export PATH=/usr/local/tranquil/bin:$PATH
+
+    function grep-port {
+        lsof -n -i4TCP:$1 | grep LISTEN
+    }
+else
+    function grep-port {
+        netstat -pntl | grep $1
+    }
+fi
+
+
+# Source module files.
+source "${0:h}/alias.zsh"
