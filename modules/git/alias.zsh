1a9ccc3addfca0921135ebcd1f85284148b07f15
Chris McDevitt <christophermcdevitt@gmail.com>
2013-11-18 18:41:43 +0000

    return before creating 100s of git aliases

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -29,6 +29,8 @@ zstyle -s ':prezto:module:git:status:ignore' submodules '_git_status_ignore_subm
 # Git
 alias g='git'
 
+return # too many aliases
+
 # Branch (b)
 alias gb='git branch'
 alias gbc='git checkout -b'
