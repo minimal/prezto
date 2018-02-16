ae747a104c59a74a7e2d391364b66cb47f37a6d3
Chris McDevitt <christophermcdevitt@gmail.com>
2018-02-16 23:39:08 +0000

    remove junk perl & opam exports

--- modules/chris-settings/exports.zsh
+++ modules/chris-settings/exports.zsh
@@ -11,15 +11,6 @@ export PROJECT_HOME=~/code
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 export PATH=".bundle/bin:$PATH"
 
-
-
-# OPAM configuration
-. $HOME/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
-CAML_LD_LIBRARY_PATH="$HOME/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs"; export CAML_LD_LIBRARY_PATH;
-PERL5LIB="$HOME/.opam/system/lib/perl5:$HOME/perl5/lib/perl5:"; export PERL5LIB;
-OCAML_TOPLEVEL_PATH="$HOME/.opam/system/lib/toplevel"; export OCAML_TOPLEVEL_PATH;
-MANPATH="$HOME/.opam/system/man:"; export MANPATH;
-
 export PATH="$HOME/.local/bin:$PATH"
 export PATH=$PATH:$HOME/Downloads/confluent-4.0.0/bin/
 
